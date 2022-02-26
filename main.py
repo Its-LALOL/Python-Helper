b69a2acf49df0c33d55ca6af47d4adf87080e03a
LALOL <89741263+Its-LALOL@users.noreply.github.com>
2022-02-26 05:07:34 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+from random import choice
+from time import sleep
+from os import name as osname, system
+
+def random_chars(amount, english_chars=True, russian_chars=False, numbers=True, spec_chars=False, big_chars=True, litte_chars=True):
+	text=''
+	chars=[]
+	if english_chars==True:
+		if big_chars==True:
+			chars+=list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+		if litte_chars==True:
+			chars+=list('abcdefghijklmnopqrstuvwxyz')
+	if russian_chars==True:
+		if big_chars==True:
+			chars+=list('АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ')
+		if litte_chars==True:
+			chars+=list('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')
+	if numbers==True:
+		chars+=list('1234567890')
+	if spec_chars==True:
+		chars+=list('~`!@#$%^&*()-_+={}[]|\/:;"\'<>,.?')
+	for i in range(amount):
+		text+=choice(chars)
+	return text
+def type(text, speed=0.1):
+	for i in text:
+		print(i, end='', flush=True)
+		sleep(speed)
+	print()
+def clear():
+	if osname=='nt' or osname=='dos':
+		system('cls')
+		return
+	system('clear')
